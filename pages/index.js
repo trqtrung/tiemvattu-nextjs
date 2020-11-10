b33621f083fb7fa964216f930e3ea1a24abec42c
Tran Quoc Trung <tranquoctrung.1610@gmail.com>
2020-11-10 20:43:13 +0700

    add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,218 @@
-import Head from 'next/head'
-import Button from '@material-ui/core/Button'
-import Skeleton from '@material-ui/lab/Skeleton'
-import Typography from '@material-ui/core/Typography'
-import useSWR from 'swr'
-import Link from 'next/link'
-import fetch from 'node-fetch'
-import https from 'https'
-import Layout from '../components/layout'
-
-export async function getStaticProps() {
-  //const https = require('https');
-
-const httpsAgent = new https.Agent({
-      rejectUnauthorized: false,
-    });
-    let data = [];
-  // Get external data from the file system, API, DB, etc.
-  try{
-  const res = await fetch('https://tiemvattu.com/api/products/homepage',{
-    method:'GET',
-    agent:httpsAgent,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  })
-  //console.log('response day ne ',res)
-  data = await res.json();
-}catch(err){
-  console.log('error',err)
-}
-
-  // The value of the `props` key will be
-  //  passed to the `Home` component
-  return {
-    props: {
-      data  
-    }
-  }
-}
-
-export default function Home({data}) {
-  return (
-    <Layout>      
-      <Head>
-        <title>Tiệm Vật Tư</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' trqtrung '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-        <Copyright/>
-      </footer>      
-    </Layout>
-  )
-}
-
-//React hook for data fetching called SWR
-//highly recommend it if you’re fetching data on the client side. It handles caching, revalidation, focus tracking, refetching on interval, and more
-// function Products(){
-//   const fetcher = (...args) => fetch(...args).then(res => res.json())
-//   const {data,error} = useSWR('https://www.tiemvattu.com/api/products/homepage', fetcher)
-
-//   if(error) return <div>failed to load...</div>
-//   if(!data) return <div>loading...</div>
-
-//   return <div>Có {data.length} sản phẩm</div>
-
-// }
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link target="_blank" href="https://tiemvattu.com">
-        <a>tiemvattu.com</a>
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-
+const Home = () => (
+  <div className="bg-white">
+    {/* <div className="fixed right-0 top-0 max-w-xs w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300 translate-x-0 ease-out">
+      <div className="flex">
+        <img
+          className="h-20 w-20 object-cover rounded"
+          src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
+          alt=""
+        ></img>
+        <div className="mx-3">
+          <h3 className="text-sm text-gray-600">Mac Book Pro</h3>
+          <div className="flex items-center mt-2">
+            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+              </svg>
+            </button>
+            <span className="text-gray-700 mx-2">2</span>
+            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <span className="text-gray-600">20$</span>
+    </div> */}
+    <main className="my-8">
+      <div className="container mx-auto px-6">
+        <h3 className="text-gray-700 text-2xl font-medium">Sản phẩm mới</h3>
+        <span className="mt-3 text-sm text-gray-500">20+ Products</span>
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+          <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+            <div
+              className="flex items-end justify-end h-56 w-full bg-cover"
+              style={{
+                backgroundImage:
+                  "url('https://tiemvattu.com/api/Files/GetFile?path=/img/product/e5b340f8eb494d47a8bf8afa5f439acd_2020-08-12_05-00-05_thumbnail.jpeg')",
+              }}
+            >
+              <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="px-5 py-3">
+              <h3 className="text-gray-700 uppercase">Khoá cửa tay nắm tròn màu nâu đen Solex Thái Lan 9570BN</h3>
+              <span className="text-gray-500 mt-2">$123</span>
+            </div>
+          </div>
+          <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+            <div
+              className="flex items-end justify-end h-56 w-full bg-cover"
+              style={{
+                backgroundImage:
+                  "url('https://tiemvattu.com/api/Files/GetFile?path=/img/product/b788b2011c2f4fa8b75fb91244a2e3e4_2019-04-29_07-04-44_thumbnail.jpg')",
+              }}
+            >
+              <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="px-5 py-3">
+              <h3 className="text-gray-700 uppercase">Kéo cắt cành 8inch TOP TG-1128</h3>
+              <span className="text-gray-500 mt-2">$123</span>
+            </div>
+          </div>
+          <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+            <div
+              className="flex items-end justify-end h-56 w-full bg-cover"
+              style={{
+                backgroundImage:
+                  "url('https://tiemvattu.com/api/Files/GetFile?path=/img/product/05562fc9e0284f05b82b489aee30fb5e_2020-08-31_03-18-18_thumbnail.jpeg')",
+              }}
+            >
+              <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="px-5 py-3">
+              <h3 className="text-gray-700 uppercase">Bộ lục giác xếp Asaki AK-7980</h3>
+              <span className="text-gray-500 mt-2">$123</span>
+            </div>
+          </div>
+          <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+            <div
+              className="flex items-end justify-end h-56 w-full bg-cover"
+              style={{
+                backgroundImage:
+                  "url('https://tiemvattu.com/api/Files/GetFile?path=/img/product/e5b340f8eb494d47a8bf8afa5f439acd_2020-08-12_05-00-05_thumbnail.jpeg')",
+              }}
+            >
+              <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="px-5 py-3">
+              <h3 className="text-gray-700 uppercase">Khoá cửa tay nắm tròn màu nâu đen Solex Thái Lan 9570BN</h3>
+              <span className="text-gray-500 mt-2">$123</span>
+            </div>
+          </div>
+          <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+            <div
+              className="flex items-end justify-end h-56 w-full bg-cover"
+              style={{
+                backgroundImage:
+                  "url('https://tiemvattu.com/api/Files/GetFile?path=/img/product/b788b2011c2f4fa8b75fb91244a2e3e4_2019-04-29_07-04-44_thumbnail.jpg')",
+              }}
+            >
+              <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="px-5 py-3">
+              <h3 className="text-gray-700 uppercase">Kéo cắt cành 8inch TOP TG-1128</h3>
+              <span className="text-gray-500 mt-2">$123</span>
+            </div>
+          </div>
+          <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+            <div
+              className="flex items-end justify-end h-56 w-full bg-cover"
+              style={{
+                backgroundImage:
+                  "url('https://tiemvattu.com/api/Files/GetFile?path=/img/product/05562fc9e0284f05b82b489aee30fb5e_2020-08-31_03-18-18_thumbnail.jpeg')",
+              }}
+            >
+              <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="px-5 py-3">
+              <h3 className="text-gray-700 uppercase">Bộ lục giác xếp Asaki AK-7980</h3>
+              <span className="text-gray-500 mt-2">$123</span>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </main>
+  </div>
+);
+export default Home;
